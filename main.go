5cbde1b25fb6f6b7dd4166dd76879870f3774e57
Dubjay18 <jejeniyi7@gmail.com>
2024-03-09 21:36:19 +0100

    feat: Json encoder

--- main.go
+++ main.go
@@ -1,13 +1,34 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
 )
 
+type Customer struct {
+	Name    string `json:"fullname"`
+	City    string `json:"city"`
+	Zipcode string `json:"zipcode"`
+}
+
 func main() {
-	http.HandleFunc("/greet", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println(w, "hello world!!")
-	})
-	http.ListenAndServe(":8080", nil)
+	http.HandleFunc("/greet", greet)
+	http.HandleFunc("/customers", getAllCustomers)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func greet(w http.ResponseWriter, r *http.Request) {
+	fmt.Println(w, "hello world!!")
+}
+func getAllCustomers(w http.ResponseWriter, r *http.Request) {
+	customers := []Customer{
+		{Name: "John", City: "New York", Zipcode: "10001"},
+		{Name: "John", City: "New York", Zipcode: "10001"},
+		{Name: "John", City: "New York", Zipcode: "10001"},
+	}
+	w.Header().Add("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(customers)
+
 }
